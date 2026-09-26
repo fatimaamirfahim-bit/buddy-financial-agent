6b7cf031b9ff01ca9fdaf1699d8fa4abca69aaeb
Fatima Amir Fahim <fahimfatimaamir@gmail.com>
2026-09-26 02:38:11 +0100

    Modified the logic

--- logic.py
+++ logic.py
@@ -11,18 +11,19 @@ savings = income * 0.2
 # Define data and labels for the chart
 values = [needs, wants, savings]
 labels = [
-    f"Needs:£{int(needs)} (50%)",
-    f"Wants: £{int(wants)} (30%)",
-    f"Savings: £{int(savings)} (20%)"
+    f"Needs:£{int(needs)}",
+    f"Wants: £{int(wants)}",
+    f"Savings: £{int(savings)}"
         ]
 colors = ["#4CAF50", "#FF9800", "#2196F3"]
 
 # Create and show the pie chart
-plt.pie(
-    values, 
-    labels=labels, 
+plt.pie(values, 
+    labels=labels,
+    autopct='%1.0f%%',
     colors=colors, 
     startangle=140
-)
+    )
+     
 plt.title(f"Budget Breakdown for £{income: .2f}")
 plt.show()
